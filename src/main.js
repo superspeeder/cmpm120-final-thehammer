f320531108e784177a2bb47ccafb803052744a11
Josh Acosta <jodacost@ucsc.edu>
2025-03-05 12:43:23 -0800

    Added font files and updated filenames

--- src/main.js
+++ src/main.js
@@ -2,13 +2,41 @@
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 900,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
     scene: [Load, Menu, LevelTen, LevelFinal],
     scale: {
         mode: Phaser.Scale.NONE,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
+}
+
+const titleTextConfig = {
+    fontFamily: 'headerBold',
+    fontsize: '36px',
+    color: '#ff0000',
+    align: 'center',
+    padding: {
+        top: 5,
+        bottom: 5
+    }
+}
+
+const scoreTextConfig = {
+    fontFamily: 'pixel',
+    fontSize: '10px',
+    color: '#000000',
+    backgroundColor: '#ffff00',
+    padding: {
+        top: 5,
+        bottom: 5
+    }
 }
 
 const game = new Phaser.Game(config)
